ff5e156e9ee0e5693220afc1f895b4020a84522d
rahulpro1012 <rahulmudaliar1012@gmail.com>
2025-08-16 02:33:15 +0530

    Initial configuration

---
+++ boardify-service/src/main/java/com/boardify/boardify_service/BoardifyApplication.java
@@ -0,0 +1,13 @@
+package com.boardify.boardify_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardifyApplication.class, args);
+	}
+
+}
---
+++ boardify-service/src/test/java/com/boardify/boardify_service/BoardifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardify.boardify_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
