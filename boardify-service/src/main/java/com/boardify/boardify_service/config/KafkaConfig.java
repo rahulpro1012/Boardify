7654d4b89afb97712e432ada5c71df5582f8017a
rahulpro1012 <rahulmudaliar1012@gmail.com>
2026-01-04 17:58:54 +0530

    Added Docker compose file

--- boardify-service/src/main/java/com/boardify/boardify_service/config/KafkaConfig.java
+++ boardify-service/src/main/java/com/boardify/boardify_service/config/KafkaConfig.java
@@ -2,6 +2,7 @@ package com.boardify.boardify_service.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonSerializer;
@@ -12,10 +13,13 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // override with property
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers); // override with property
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         // JsonSerializer will add type headers; fine for internal services
